8917016a8c9e9c71c1990c0ff2de2f25f963e9ad
Alex McRitchie <amcritchie@gmail.com>
2016-09-05 22:47:21 -0600

    Removing from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,13 +56,3 @@ namespace :deploy do
     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
 end
-
-# Capistrano::Configuration.instance(:must_exist).load do
-  namespace :rails do
-    desc "Open the rails console on one of the remote servers"
-    task :console, :roles => :app do
-      hostname = find_servers_for_task(current_task).first
-      exec "ssh -l #{user} #{hostname} -t 'source ~/.profile && #{current_path}/script/rails c #{rails_env}'"
-    end
-  end
-# end
