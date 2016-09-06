0e8188cfd17107427b8a9e061086bd3e65438cdc
Alex McRitchie <amcritchie@gmail.com>
2016-09-05 21:55:14 -0600

    Creating admin pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'picks#public' # The latest weekly lock.
+
+  get 'pick_latest' => 'weeks#pick_latest' # Propt for user to pick a game from the latest week.
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/falure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
-  get 'pick_latest' => 'weeks#pick_latest'
-
-  # You can have the root of your site routed with "root"
-  root 'picks#public'
+  get 'admin' => 'admin#sport'
+  get 'admin/nfl' => 'admin#sport'
+  get 'admin/:sport/:year' => 'admin#season'
+  get 'admin/:sport/:year/:week' => 'admin#week'
+  get 'admin/:sport/:year/:week/:slug' => 'admin#game'
 
   get 'sport/:sport/season/:year/week/:week/public' => 'picks#public'
 
