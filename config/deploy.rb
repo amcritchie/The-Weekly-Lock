1d691be8ac5d737034878f722e8747ef60057778
Alex McRitchie <amcritchie@gmail.com>
2016-09-07 00:49:11 -0600

    Testing a deploy stratagy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,16 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
+  desc "SCP transfer figaro configuration to the shared folder"
+  task :setup do
+    transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
+  end
+
+  desc "Symlink application.yml to the release path"
+  task :finalize do
+    run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
+  end
+
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
