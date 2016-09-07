6bab88ddb373bc505484a348d3b8ac087e196d5f
Alex McRitchie <amcritchie@gmail.com>
2016-09-07 00:44:40 -0600

    Adding figaro deploy task.

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,66 +50,14 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
 end
 
-# namespace :deploy do
-#   desc "reload the database with seed data"
-#   # rake db:drop db:create db:migrate db:seed
-#
-#   task :full do
-#     run "cd #{current_path}; bundle exec rake db:reset RAILS_ENV=#{rails_env}"
-#     run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
-#     run "cd #{current_path}; bundle exec rake db:migrate RAILS_ENV=#{rails_env}"
-#     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-#   end
-#   task :seed do
-#     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-#   end
-# end
+namespace :figaro do
+  desc "SCP transfer figaro configuration to the shared folder"
+  task :setup do
+    transfer :up, "config/application.yml", "#{shared_path}/application.yml", :via => :scp
+  end
 
-# namespace :deploy do
-#   namespace :assets do
-#
-#     task :precompile, :roles => :web do
-#       from = source.next_revision(current_revision)
-#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-#         run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
-#         run_locally "cd public && tar -jcf assets.tar.bz2 assets"
-#         top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
-#         run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-#         run_locally "rm public/assets.tar.bz2"
-#         run_locally("rake assets:clean")
-#       else
-#         logger.info "Skipping asset precompilation because there were no asset changes"
-#       end
-#     end
-#
-#     task :symlink, roles: :web do
-#       run ("rm -rf #{latest_release}/public/assets &&
-#       mkdir -p #{latest_release}/public &&
-#       mkdir -p #{shared_path}/assets &&
-#       ln -s #{shared_path}/assets #{latest_release}/public/assets")
-#     end
-#   end
-#
-#   # other stuff...
-#   # namespace :rake do
-#   #   namespace :db do
-#   #     %w[create migrate reset rollback seed setup].each do |command|
-#   #       desc "Rake db:#{command}"
-#   #       task command, roles: :app, except: {no_release: true} do
-#   #         run "cd #{deploy_to}/current"
-#   #         run "bundle exec rake db:#{ENV['task']} RAILS_ENV=#{rails_env}"
-#   #       end
-#   #     end
-#   #   end
-#   #   namespace :assets do
-#   #     %w[precompile clean].each do |command|
-#   #       desc "Rake assets:#{command}"
-#   #       task command, roles: :app, except: {no_release: true} do
-#   #         run "cd #{deploy_to}/current"
-#   #         run "bundle exec rake assets:#{ENV['task']} RAILS_ENV=#{rails_env}"
-#   #       end
-#   #     end
-#   #   end
-#   # end
-#
-# end
+  desc "Symlink application.yml to the release path"
+  task :finalize do
+    run "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
+  end
+end
